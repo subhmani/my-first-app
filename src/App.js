f3a73effcfbca99902408248ec4531cb50940367
Subhash Mani <subhmani4631@gmail.com>
2024-03-26 18:44:01 +0530

    Implementing JS Code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 import Subscription from './components/Subscription';
 
 function App() {
+  let subscriptions=[
+    {
+      id:"1",
+      date:(new Date('2024','03','23')),
+      title:"Monthly Subscription",
+      amount:"125.60"
+    },
+    {
+      id:"2",
+      date:(new Date('2024','06','28')),
+      title:"Annual Subscription",
+      amount:"1125.00"
+    },
+    {
+      id:"3",
+      date:(new Date('2024','09','05')),
+      title:"Quarterly Subscription",
+      amount:"425.50"
+    }
+]
+    /* let date=(new Date('2024','03','26'));
+    let title="Monthly Subscription";
+    let amount='125.60'; */
   return (
-    <div className="App">
-      <header className="App-header">
-      <Subscription/>
+    <div>
+    <Subscription date={subscriptions[0].date} title={subscriptions[0].title} amount={subscriptions[0].amount}/>
+    <Subscription date={subscriptions[1].date} title={subscriptions[1].title} amount={subscriptions[1].amount}/>
+    <Subscription date={subscriptions[2].date} title={subscriptions[2].title} amount={subscriptions[2].amount}/>
 
-     
-      </header>
     </div>
   );
 }
--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,6 +1,16 @@
-function Subscription(){
+import './Subscription.css'
+function Subscription(props){
+    const month=props.date.toLocaleString('default',{month:'long'});
+    const day=props.date.toLocaleString('default',{day:'2-digit'});
+    const year=props.date.getFullYear();
     return(
-    <h1>Subscription Title</h1>
+    <div className='subscription'>
+        <div> {month} </div>
+        <div> {day} </div>
+        <div> {year} </div>
+        <h2 className='subscription_title'>{props.title}</h2>
+        <div className='subscription_price'>{props.amount}</div>
+    </div>
     )
 }
 export default Subscription
