b88973ef71ea32812a9ef75b52968e3ca91e64ea
Subhash Mani <subhmani4631@gmail.com>
2024-03-28 09:54:56 +0530

    add component subscription date

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,13 +1,9 @@
 import './Subscription.css'
+import SubscriptionDate from './SubscriptionDate'
 function Subscription(props){
-    const month=props.date.toLocaleString('default',{month:'long'});
-    const day=props.date.toLocaleString('default',{day:'2-digit'});
-    const year=props.date.getFullYear();
-    return(
+   return(
     <div className='subscription'>
-        <div> {month} </div>
-        <div> {day} </div>
-        <div> {year} </div>
+        <SubscriptionDate date={props.date}/>
         <h2 className='subscription_title'>{props.title}</h2>
         <div className='subscription_price'>{props.amount}</div>
     </div>
---
+++ src/components/SubscriptionDate.js
@@ -0,0 +1,15 @@
+import './SubscriptionDate.css';
+function SubscriptionDate(props){
+    const month=props.date.toLocaleString('default',{month:'long'});
+    const day=props.date.toLocaleString('default',{day:'2-digit'});
+    const year=props.date.getFullYear();
+    return(
+        <div className='date'>
+        <div className='month'> {month} </div>
+        <div className='day'> {day} </div>
+        <div className='year'> {year} </div>
+        </div>
+    )
+
+}
+export default SubscriptionDate
