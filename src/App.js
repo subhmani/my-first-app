6172f89d77cc7c4175b8b3db516f08ffacb8a77e
Subhash Mani <subhmani4631@gmail.com>
2024-03-31 16:17:41 +0530

    Filter Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Subscription from './Subscription/Subscription';
 import Container from './templates/Container';
 import React from 'react';
+import Filter from './Subscription/NewSubscription/Filter';
 import NewSubscription from './Subscription/NewSubscription/NewSubscription';
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
 
  const App = () => {
   let subscriptions=[
@@ -33,11 +35,15 @@ import NewSubscription from './Subscription/NewSubscription/NewSubscription';
       subscriptions.push(data)
       console.log("on add Subscription",data)
     }
+    const filterChangeHandler=(data)=>{
+      console.log('filter change handler',data)
+    }
   return (
     // React.createElement('div',{},React.createElement('h2',{},'Lets start!!'))
     <Container>
     <Subscription date={subscriptions[0].date} title={subscriptions[0].title} amount={subscriptions[0].amount}/>
     <NewSubscription onAddSubscription={addSubscriptionHandler}/>
+    <Filter onFilterChange={filterChangeHandler}/>
     <Subscription date={subscriptions[1].date} title={subscriptions[1].title} amount={subscriptions[1].amount}/>
     <Subscription date={subscriptions[2].date} title={subscriptions[2].title} amount={subscriptions[2].amount}/>
 
---
+++ src/Subscription/NewSubscription/Filter.js
@@ -0,0 +1,23 @@
+import './Filter.css'
+const Filter=(props)=>{
+    const onSelectHandler=(events)=>{
+        console.log('onSelect Handler',events.target.value)
+        props.onFilterChange(events.target.value)
+    }
+    return(
+        <div className='filter'>
+            <div className='filter_control'>
+                <label>Filter by Year</label>
+                <select onChange={onSelectHandler}>
+                    <option value='2024'>2024</option>
+                    <option value='2023'>2023</option>
+                    <option value='2022'>2022</option>
+                    <option value='2021'>2021</option>
+                </select>
+
+            </div>
+
+        </div>
+    )
+}
+export default Filter
