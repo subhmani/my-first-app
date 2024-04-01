8b96d13204c1e1f86e5272c484de6a81e3e264e0
Subhash Mani <subhmani4631@gmail.com>
2024-04-01 12:18:29 +0530

    Array in react state

--- src/App.js
+++ src/App.js
@@ -6,35 +6,34 @@ import React, { useState } from 'react';
 import Filter from './Subscription/NewSubscription/Filter';
 import NewSubscription from './Subscription/NewSubscription/NewSubscription';
 import { eventWrapper } from '@testing-library/user-event/dist/utils';
-
+const INITIAL_SUBSCRIPTION=[{
+  id:"1",
+  date:(new Date('2024','03','23')),
+  title:"Monthly Subscription",
+  amount:"125.60"
+},
+{
+  id:"2",
+  date:(new Date('2024','06','28')),
+  title:"Annual Subscription",
+  amount:"1125.00"
+},
+{
+  id:"3",
+  date:(new Date('2024','09','05')),
+  title:"Quarterly Subscription",
+  amount:"425.50"
+}]
  const App = () => {
-  let subscriptions=[
-    {
-      id:"1",
-      date:(new Date('2024','03','23')),
-      title:"Monthly Subscription",
-      amount:"125.60"
-    },
-    {
-      id:"2",
-      date:(new Date('2024','06','28')),
-      title:"Annual Subscription",
-      amount:"1125.00"
-    },
-    {
-      id:"3",
-      date:(new Date('2024','09','05')),
-      title:"Quarterly Subscription",
-      amount:"425.50"
-    }
-]
+  const [subscriptions,setSubscriptions]=useState(INITIAL_SUBSCRIPTION)
 const [filteredData,setFilteredData]=useState('2021');
     /* let date=(new Date('2024','03','26'));
     let title="Monthly Subscription";
     let amount='125.60'; */
     const addSubscriptionHandler=(data)=>{
       subscriptions.push(data)
-      console.log("on add Subscription",data)
+      setSubscriptions(prevState=>{return[data,...subscriptions]})
+      console.log("on add Subscription",subscriptions)
     }
     const filterChangeHandler=(data)=>{
       setFilteredData(data);
@@ -46,7 +45,7 @@ const [filteredData,setFilteredData]=useState('2021');
     <Container>
     <NewSubscription onAddSubscription={addSubscriptionHandler}/>
     <Filter onFilterChange={filterChangeHandler} selectedFilter={filteredData}/>
-    {subscriptions.map(subscription =><Subscription date={subscription.date} title={subscription.title} amount={subscription.amount}/>)}
+    {subscriptions.map(subscription =><Subscription key={subscription.id} date={subscription.date} title={subscription.title} amount={subscription.amount}/>)}
     
     {/* <Subscription date={subscriptions[0].date} title={subscriptions[0].title} amount={subscriptions[0].amount}/>
     <Subscription date={subscriptions[1].date} title={subscriptions[1].title} amount={subscriptions[1].amount}/>
