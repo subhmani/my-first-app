14575462544c28e60a17ebcc41f69b376662f837
Subhash Mani <subhmani4631@gmail.com>
2024-04-06 18:00:04 +0530

    adding handler

--- src/Subscription/NewSubscription/NewSubscription.js
+++ src/Subscription/NewSubscription/NewSubscription.js
@@ -1,12 +1,22 @@
 import "./NewSubscription.css"
 import FormSubscription from './FormSubscription';
+import { useState } from "react";
 const NewSubscription=(props)=>{
+    const [showForm,setShowForm]=useState(false)
  const  onSaveHandler = (data) => {
         const subscriptionData={...data,id:Math.random().toString()}
         props.onAddSubscription(subscriptionData);
         console.log("on save", subscriptionData)
     }
-    return <div className="new_subscription"><FormSubscription onSave={onSaveHandler}/></div>
+    const showFormHandler=()=>{
+        setShowForm(true)
+        console.log('show form',showForm)
+    }
+    return <div className="new_subscription">
+       {showForm && <FormSubscription onSave={onSaveHandler}/>} 
+        <button type="button" onClick={showFormHandler}>Add New</button>
+        </div>
+        
     
 }
 export default NewSubscription
