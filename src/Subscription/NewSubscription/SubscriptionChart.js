bf4cbd211c0950cd2d04af20d33e1e5cb83a98ca
Subhash Mani <subhmani4631@gmail.com>
2024-04-09 20:55:07 +0530

    Creating Dataset

---
+++ src/Subscription/NewSubscription/SubscriptionChart.js
@@ -0,0 +1,25 @@
+import './SubscriptionChart.css'
+import Chart from '../../templates/charts/Chart';
+const SubscriptionChart=(props)=>{
+    const chartData=[{label:'Jan',value:0},
+                    {label:'Feb',value:0},
+                    {label:'Mar',value:0},
+                    {label:'Apr',value:0},
+                    {label:'May',value:0},
+                    {label:'Jun',value:0},
+                    {label:'Jul',value:0},
+                    {label:'Aug',value:0},
+                    {label:'Sep',value:0},
+                    {label:'Oct',value:0},
+                    {label:'Nov',value:0},
+                    {label:'Dec',value:0}];
+     for(const subscription of props.filteredSubscription){
+        const month=subscription.data.getMonth;
+        chartData[month].value += subscription.amount
+     }               
+    return (
+        <Chart dataSets={chartData}/>
+    )
+
+}
+export default SubscriptionChart;
