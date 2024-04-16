d61dd1e4d728dc51089f7cf18a3a75ef76900b6b
Subhash Mani <subhmani4631@gmail.com>
2024-04-16 10:56:36 +0530

    Adding Scopes

--- src/templates/Container.js
+++ src/templates/Container.js
@@ -1,7 +1,9 @@
 import './Container.css';
 import '../Subscription/Subscription.css';
- const Container = (props) => {
+import styled from './styled-components';
+ /* const Container = (props) => {
     const classes=" container " + props.className
     return <div className={classes}>{props.children}</div>
-}
+} */
+const Container=styled.div``; styled.button``; styled.span``;
 export default Container
