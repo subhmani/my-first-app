74355c7fbb8e4248e4b23ca8d0ecc6148bfa5352
Subhash Mani <subhmani4631@gmail.com>
2024-04-16 20:06:27 +0530

    style modula

--- src/templates/Container.js
+++ src/templates/Container.js
@@ -1,14 +1,14 @@
-import './Container.css';
+import styles from './Container.module.css';
 import '../Subscription/Subscription.css';
 import styled from 'styled-components';
- /* const Container = (props) => {
-    const classes=" container " + props.className
-    return <div className={classes}>{props.children}</div>
-} */
-const Container=styled.div`{
+ const Container = (props) => {
+    //const classes="styles.container"  + props.className
+    return <div className={styles.container}>{props.children}</div>
+}
+/* const Container=styled.div`{
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
     border-radius: 12px;
 }
 &:hover{border:red}
-`; 
+`;  */
 export default Container
