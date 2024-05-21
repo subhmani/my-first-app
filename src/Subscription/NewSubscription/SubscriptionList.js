b8947743cc87dcab006613627873fbacecbd3177
Subhash Mani <subhmani4631@gmail.com>
2024-05-21 11:50:41 +0530

    useContext

--- src/Subscription/NewSubscription/SubscriptionList.js
+++ src/Subscription/NewSubscription/SubscriptionList.js
@@ -1,10 +1,12 @@
 import './SubscriptionList.css';
 import Subscription from '../Subscription';
 import SubscriptionsContext from '../../store/subscriptions-context';
+import { useContext } from 'react';
 const SubscriptionList=(props)=>{
 if (props.subscription.length === 0){return <h3 className='list_no_data'>No Data Found</h3>}
+const ctx = useContext(SubscriptionsContext)
 return(
-    <SubscriptionsContext.Consumer>
+    /* <SubscriptionsContext.Consumer>
         {(ctx)=>{
             <ul className='list'>
             {ctx.subscriptionList.map(subscription =>
@@ -13,7 +15,12 @@ return(
             </ul>
         }}
     
-    </SubscriptionsContext.Consumer>
+    </SubscriptionsContext.Consumer> */
+    <ul className='list'>
+    {ctx.subscriptionList.map(subscription =>
+    <Subscription key={subscription.id} date={subscription.date} 
+    title={subscription.title} amount={subscription.amount}/>)}
+    </ul>
 )
 }
 export default SubscriptionList;
